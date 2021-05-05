bc07e93eee129e9bbe146b32e0a0d6d4599e4b97
Overseer009 <sacha.sunnasy@gmail.com>
2021-05-05 13:55:12 -0400

    added my word of gameplay into the mix

--- input.js
+++ input.js
@@ -21,6 +21,12 @@ const handleUserInput = function(key) {
     connection.write('Move: down')
   } else if (key === 'd') {
     connection.write('Move: right')
+  }else if (key === 'x') {
+    connection.write("Say: Gonna Getcha")
+  } else if (key === 'z') {
+    connection.write("Say: KABOOM")
+  } else if (key === 'c') {
+    connection.write("Say: Are you kidding me!?")
   }
 }
 
